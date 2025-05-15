de5fc68e14ae99fe28e4102aa690689ad593f1d5
KrisDL75 <krisdhermyluce@gmail.com>
2025-05-15 13:38:32 +0200

    Tries on the GUI

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+
+namespace SimpleWpfApp
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MyTextBlock.Text = "Tu as cliqué !";
+        }
+    }
+}
